21ec7e7e51b14bf4a409ed2258caa1ff4a65afb5
BackupMiles <leanwithmiles@gmail.com>
2024-07-01 21:37:35 +0200

    ref: move uniforms to own file

---
+++ src/constants/uniforms.ts
@@ -0,0 +1,7 @@
+import { Vector2 } from "three";
+
+export const DEFAULT_UNIFORMS = {
+  u_time: { type: "f", value: 1.0 },
+  u_resolution: { type: "v2", value: new Vector2() },
+  u_mouse: { type: "v2", value: new Vector2() },
+}

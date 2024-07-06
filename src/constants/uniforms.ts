f3f937941ba8dbcd72ed5ef357fbe558c6f42ef9
BackupMiles <leanwithmiles@gmail.com>
2024-07-07 01:31:10 +0200

    feat: add mouse_enabled uniform

--- src/constants/uniforms.ts
+++ src/constants/uniforms.ts
@@ -1,7 +1,15 @@
 import { Vector2 } from "three";
 
-export const DEFAULT_UNIFORMS = {
+export type TUniforms = {
+  u_mouse_enabled: any;
+  u_mouse: any;
+  u_resolution: any;
+  u_time: any;
+}
+
+export const DEFAULT_UNIFORMS: TUniforms = {
   u_time: { type: "f", value: 1.0 },
   u_resolution: { type: "v2", value: new Vector2() },
   u_mouse: { type: "v2", value: new Vector2() },
+  u_mouse_enabled: { type: "b", value: false },
 }

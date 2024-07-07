bd6133d594a656c0de9ef4da1ff0059761b79061
BackupMiles <leanwithmiles@gmail.com>
2024-07-07 16:45:41 +0200

    feat: add noise to elevation

--- src/components/examples/raging-sea/uniforms.tsx
+++ src/components/examples/raging-sea/uniforms.tsx
@@ -1,19 +1,33 @@
-import { IUniform, Vector2 } from "three";
+import { Color, IUniform, Vector2 } from "three";
 import { DEFAULT_UNIFORMS, TUniforms } from "../../../constants/uniforms";
 
 export type TWavesUniform = TUniforms & {
-  u_depth_color: IUniform<string>;
-  u_surface_color: IUniform<string>;
   u_big_waves_speed: IUniform<number>;
   u_big_waves_elevation: IUniform<number>;
   u_big_waves_frequency: IUniform<Vector2>;
+  u_noise_amplifier: IUniform<number>;
+  u_waves_multiplier: IUniform<number>;
+  // Color
+  u_color_offset: IUniform<number>;
+  u_color_multiplier: IUniform<number>;
+  u_depth_color: IUniform<Color>;
+  u_surface_color: IUniform<Color>;
 };
 
-export const WAVES_UNIFORM: TWavesUniform = {
+type TWavesBuilderOptions = {
+  depthColor: string;
+  surfaceColor: string;
+}
+
+export const buildWavesUniform = ({ depthColor, surfaceColor }: TWavesBuilderOptions): TWavesUniform => ({
   ...DEFAULT_UNIFORMS,
+  u_color_multiplier: { value: 5.0 },
+  u_color_offset: { value: 0.25 },
   u_big_waves_elevation: { value: 0.2 },
   u_big_waves_frequency: { value: new Vector2(4, 1.5) },
   u_big_waves_speed: { value: 0.75 },
-  u_depth_color: { value: "#0000ff" },
-  u_surface_color: { value: "#8888ff" },
-};
+  u_depth_color: { value: new Color(depthColor) },
+  u_noise_amplifier: { value: 0.2 },
+  u_surface_color: { value: new Color(surfaceColor) },
+  u_waves_multiplier: { value: 3.0 },
+});
--- src/components/examples/raging-sea/utils.tsx
+++ src/components/examples/raging-sea/utils.tsx
@@ -2,31 +2,70 @@ import GUI from "lil-gui";
 import { TWavesUniform } from "./uniforms";
 
 export const initializeGUI = (gui: GUI, uniforms: TWavesUniform) => {
-  gui
+  const waves = gui.addFolder("waves");
+  waves
     .add(uniforms.u_big_waves_elevation, "value")
     .min(0)
     .max(1)
     .step(0.001)
-    .name("uBigWavesElevation");
+    .name("u_big_waves_elevation");
 
-  gui
+  waves
     .add(uniforms.u_big_waves_frequency.value, "x")
     .min(0)
     .max(10)
     .step(0.001)
-    .name("uBigWavesFrequencyX");
+    .name("u_big_waves_frequency_x");
 
-  gui
+  waves
     .add(uniforms.u_big_waves_frequency.value, "y")
     .min(0)
     .max(10)
     .step(0.001)
-    .name("uBigWavesFrequencyY");
+    .name("u_big_waves_frequency_y");
 
-  gui
+  waves
     .add(uniforms.u_big_waves_speed, "value")
     .min(0)
     .max(4)
     .step(0.001)
-    .name("uBigWavesSpeed");
+    .name("u_big_waves_speed");
+
+  waves
+    .add(uniforms.u_noise_amplifier, "value")
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name("u_noise_amplifier");
+
+  waves
+    .add(uniforms.u_waves_multiplier, "value")
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name("u_waves_multiplier");
+
+  // colors
+  const color = gui.addFolder("color");
+  color
+    .addColor(uniforms.u_depth_color, "value")
+    .name("u_depth_color");
+
+  color
+    .addColor(uniforms.u_surface_color, "value")
+    .name("u_surface_color");
+
+  color
+    .add(uniforms.u_color_offset, "value")
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name("u_color_offset");
+
+  color
+    .add(uniforms.u_color_multiplier, "value")
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name("u_color_multiplier");
 };

e9fe697879bb3ffd03e2bc3bf0dd8ac976c8989b
BackupMiles <leanwithmiles@gmail.com>
2024-07-07 15:35:04 +0200

    feat: add custom color-based depth

--- src/assets/raging-sea/fragment.glsl
+++ src/assets/raging-sea/fragment.glsl
@@ -1,4 +1,8 @@
+varying vec4 aPosition;
+
 void main() {
     #include <colorspace_fragment>
-    gl_FragColor = vec4(0., 0.9, 1., 1.);
+    vec3 baseColors = vec3(0., 0.9, 1.);
+    baseColors.xyz += aPosition.z;
+    gl_FragColor = vec4(baseColors, 1.);
 }

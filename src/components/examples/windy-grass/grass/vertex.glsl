0c1af5b350a695cad77b6c119dc688cd230aeddc
BackupMiles <leanwithmiles@gmail.com>
2024-07-13 20:46:34 +0200

    feat: adding wind test

--- src/components/examples/windy-grass/grass/vertex.glsl
+++ src/components/examples/windy-grass/grass/vertex.glsl
@@ -1,6 +1,15 @@
+uniform float u_time;
+
+varying vec2 vUv;
+
 void main() {
   vec4 mv = modelViewMatrix * vec4(position, 1.);
   vec4 p = projectionMatrix * mv;
 
+  if (uv.y > 0.5) {
+    p.x -= abs(sin(u_time));
+  }
+
   gl_Position = p;
+  vUv = uv;
 }

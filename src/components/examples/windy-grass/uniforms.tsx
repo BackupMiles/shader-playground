7215ef92d814cea8ce75ab01e9e88ca3b671a2c7
BackupMiles <leanwithmiles@gmail.com>
2024-07-14 18:06:36 +0200

    feat: add bend factor

--- src/components/examples/windy-grass/uniforms.tsx
+++ src/components/examples/windy-grass/uniforms.tsx
@@ -2,12 +2,14 @@ import { Color, IUniform } from "three";
 import { DEFAULT_UNIFORMS, TUniforms } from "../../../constants/uniforms";
 
 export type TGrassUniforms = TUniforms & {
+  u_bend_factor: IUniform<Number>;
   u_height: IUniform<Number>;
   u_color: IUniform<Color>;
 };
 
-export const buildGrassUniform = ({color, height} : {color: string, height: number}): TGrassUniforms => ({
+export const buildGrassUniform = ({bend, color, height} : {bend:number, color: string, height: number}): TGrassUniforms => ({
   ...DEFAULT_UNIFORMS,
+  u_bend_factor: { value: bend },
   u_color: { value: new Color(color) },
   u_height: { value: height }
 });
--- src/components/examples/windy-grass/utils.tsx
+++ src/components/examples/windy-grass/utils.tsx
@@ -4,4 +4,5 @@ import { TGrassUniforms } from "./uniforms";
 export const initializeGUI = (gui: GUI, uniforms: TGrassUniforms) => {
   gui.addColor(uniforms.u_color, "value").name("u_color");
   gui.add(uniforms.u_height, "value").min(1).max(10).name("u_height");
+  gui.add(uniforms.u_bend_factor, "value").min(-10).max(10).name("u_bend_factor");
 }
